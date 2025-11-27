d0740252d0a6a51f12fbd1b6c0b06eb9daed52a2
Matthew Rock <rock.ma@Northeastern.edu>
2025-11-27 00:28:12 -0500

    added watchdog pets

--- Physical-Layer/main/lifi_packet.c
+++ Physical-Layer/main/lifi_packet.c
@@ -23,6 +23,8 @@ void lifi_packet_init(void) {
 }
 
 void send_byte(char byte) {
+    //pet watchdog
+    esp_task_wdt_reset();
     for (int i = 0; i < 8; i++) {
         digitalWrite(LED_PIN, byte & 1);
         byte >>= 1;
@@ -41,6 +43,8 @@ void send_packet_data_over_lifi(eth_packet_t *packet)
 }
 
 char receive_byte() {
+    //pet watchdog
+    esp_task_wdt_reset();
     char byte = 0;
     for (int i = 0; i < 8; i++) {
         sleep(CLOCK_TICK);
