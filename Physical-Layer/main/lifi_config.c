96bb9ba823f2656f14defa6732620c08f346baf0
Matthew Rock <thematthewrock@gmail.com>
2025-12-03 15:21:46 -0500

    testing

--- Physical-Layer/main/lifi_config.c
+++
@@ -1,8 +0,0 @@
-#include "lifi_config.h"
-
-void lifi_sleep(int microseconds) {
-    int start = esp_timer_get_time();
-    while (esp_timer_get_time() - start < microseconds) {
-        //busy wait
-    }
-}
