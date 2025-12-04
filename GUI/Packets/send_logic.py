d577a12669f1e310ea284b6e4d036d2f26c2d874
Matthew Rock <thematthewrock@gmail.com>
2025-12-04 15:09:54 -0500

    removed \00

--- GUI/Packets/send_logic.py
+++ GUI/Packets/send_logic.py
@@ -52,6 +52,7 @@ def configure_eth_if(eth_type: int, target_if: str = '') -> Iterator[socket.sock
         so.close()
 
 def requeue_dropped(message: bytes) -> None:
+    message = message.rstrip(b'\x00')
     packet = [x for x in SENT_PACKET_LIST if x.startswith(message)]
     if(len(packet) > 0):
         PACKET_QUEUE.put(packet[0])
